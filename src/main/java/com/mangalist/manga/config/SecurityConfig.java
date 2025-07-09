154467fef6c91dc5361fe60cd2314eeb615ec328
Bhuvan-Hegde <149949915+Bhuvan-Hegde@users.noreply.github.com>
2025-07-09 12:30:50 +0530

    Update SecurityConfig.java

--- src/main/java/com/mangalist/manga/config/SecurityConfig.java
+++ src/main/java/com/mangalist/manga/config/SecurityConfig.java
@@ -20,6 +20,9 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
 
+import java.util.List;
+
+
 @Configuration
 @EnableWebSecurity
 @EnableMethodSecurity
